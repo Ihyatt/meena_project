f3f85c41d4606e4971da657efb52f61a683865e6
Inas Hyatt <inas.raheema@gmail.com>
2025-06-15 04:56:48 -0700

    added enums for currency type to constants

---
+++ client/src/utils/Constants.jsx
@@ -0,0 +1,38 @@
+
+
+
+const CURRENCY_CODE = {
+    "AUD" : "AUD",
+    "EUR" : "EUR",
+    "BRL" : "BRL",
+    "CAD" : "CAD",
+    "XOF" : "XOF",
+    "HRK" : "EUR",
+    "CZK" : "CZK",
+    "DKK" : "DKK",
+    "GHS" : "GHS",
+    "GIP" : "GIP",
+    "HKD" : "HKD",
+    "HUF" : "HUF",
+    "INR" : "INR",
+    "IDR" : "IDR",
+    "JPY" : "JPY",
+    "KES" : "KES",
+    "CHF" : "CHF",
+    "MYR" : "MYR",
+    "MXN" : "MXN",
+    "NZD" : "NZD",
+    "NGN" : "NGN",
+    "NOK" : "NOK",
+    "PLN" : "PLN",
+    "RON" : "RON",
+    "SGD" : "SGD",
+    "ZAR" : "ZAR",
+    "SEK" : "SEK",
+    "THB" : "THB",
+    "AED" : "AED",
+    "GBP" : "GBP",
+    "USD" : "USD"
+}
+
+export default CURRENCY_CODE;
