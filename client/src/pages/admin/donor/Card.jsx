8e355cf83ea5f539eb211a026d25e3f871521db8
Inas Hyatt <inas.raheema@gmail.com>
2025-08-14 23:05:53 -0700

    minor adjustments to donor table

--- client/src/pages/admin/donor/Card.jsx
+++ client/src/pages/admin/donor/Card.jsx
@@ -15,31 +15,19 @@ const Donor = ({ data }) => {
     return (
         <tr key={data.id}>
             <td className="p-4 border-b border-blue-gray-50">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
+                <div className="text-xs ">
                     {data.fullName ? data.fullName : 'NA'}
-                </Typography>
+                </div>
 
             </td>
             <td className="p-4 border-b border-blue-gray-50">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
+                <div className="text-xs ">
                     {data.emailAddress ? data.emailAddress : 'NA'}
-                </Typography>
-
+                </div>
             </td>
             <td className="p-4 border-b border-blue-gray-50">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
+                <div className="text-xs ">
+
                     <NumericFormat
                         value={totalDonated}
                         thousandSeparator={true}
@@ -47,47 +35,30 @@ const Donor = ({ data }) => {
                         decimalScale={2}
                         displayType="text"
                     />
-                </Typography>
+                </div>
             </td>
 
             <td className="p-4 border-b border-blue-gray-50">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
+                <div className="text-xs ">
                     {data.donations.length}
-                </Typography>
-            </td>
-            <td className="p-4 border-b border-blue-gray-50 ">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
-                    {data.emailSubscription.queued}
-                </Typography>
-            </td>
-            <td className="p-4 border-b border-blue-gray-50 ">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
-                    {data.emailSubscription.opened}
-                </Typography>
-            </td>
-            <td className="p-4 border-b border-blue-gray-50">
-                <div className="w-max ">
-                    <Chip
-                        variant="ghost"
-                        size="sm"
-                        value={data.emailSubscription.status == "ACTIVE" ? "subscribed" : "unsubscribed"}
-                        className={`font-normal ${data.emailSubscription.status == "ACTIVE" ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}
-                    />
                 </div>
             </td>
-        </tr>
+            <td className="p-4 border-b border-blue-gray-50 ">
+                <div className="text-xs ">
+                    {data.emailSubscription.queued}
+                </div>
+            </td>
+            <td className="p-4 border-b border-blue-gray-50 ">
+                <div className="text-xs ">
+                    {data.emailSubscription.opened}
+                </div>
+            </td>
+            <td className="p-4 border-b border-blue-gray-50">
+                <div className={`py-1 px-2 rounded-md w-max text-xs  ${data.emailSubscription.status == "ACTIVE" ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>
+                    {data.emailSubscription.status == "ACTIVE" ? "subscribed" : "unsubscribed"}
+                </div>
+            </td>
+        </tr >
     );
 };
 
