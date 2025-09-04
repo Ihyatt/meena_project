028a492ea57191695c9af99cf8357073c31fc0ce
Ihyatt <inas.raheema@gmail.com>
2025-09-03 22:16:24 -0700

    image upload ui is good

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -23,23 +23,54 @@ const ImageUpload = ({ imageUrl, uploadFile }) => {
   };
 
   return (
-    <div className="mt-2 ml-1 mr-1 flex flex-col items-center justify-center">
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        style={{ display: "none" }}
-        accept="image/*"
-      />
+    <div>
+      {imageUrl ? (
+        <div>
+          <img
+            src={imageUrl}
+            alt="ui/ux review check"
+            className="rounded-sm shadow-md h-100 w-full object-cover"
+          />
 
-      <label
-        htmlFor="hiddenFileInput"
-        onClick={handleIconClick}
-        className="cursor-pointer flex flex-col items-center justify-center text-border text-[#b7b7b6] border-[#b7b7b6] border rounded-lg p-40 border-dashed"
-      >
-        <FiUploadCloud size={60} color={"#b7b7b6"} />
-        <div className="text-[#b7b7b6]">Click to Upload</div>
-      </label>
+          <div className="mt-2 ml-1 mr-1 flex flex-col items-center justify-center">
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              style={{ display: "none" }}
+              accept="image/*"
+            />
+
+            <label
+              htmlFor="hiddenFileInput"
+              onClick={handleIconClick}
+              className="cursor-pointer flex items-center text-slate-700"
+            >
+              <div className="mr-2">Replace</div>
+              <FiUploadCloud size={20} />
+            </label>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-2 ml-1 mr-1 flex flex-col items-center justify-center">
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+            accept="image/*"
+          />
+
+          <label
+            htmlFor="hiddenFileInput"
+            onClick={handleIconClick}
+            className="cursor-pointer flex flex-col items-center justify-center text-border text-[#b7b7b6] border-[#b7b7b6] border rounded-lg p-40 border-dashed"
+          >
+            <FiUploadCloud size={60} color={"#b7b7b6"} />
+            <div className="text-[#b7b7b6]">Click to Upload</div>
+          </label>
+        </div>
+      )}
     </div>
   );
 };
